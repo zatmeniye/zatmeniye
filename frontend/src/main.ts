4d22225e1f4939029160e5f397e87842ef21b975
zatmeniye <zatmeniye.zatmeniye@gmail.com>
2024-03-08 00:12:50 +0700

    построил структуру интерфейса

---
+++ frontend/src/main.ts
@@ -0,0 +1,15 @@
+import { VueQueryPlugin } from "@tanstack/vue-query";
+import { createApp } from "vue";
+import PrimeVue from "primevue/config";
+import Wind from "@/presets/wind";
+import App from "@/App.vue";
+import "@/index.css";
+
+const app = createApp(App);
+
+app.use(VueQueryPlugin).use(PrimeVue, {
+	unstyled: true,
+	pt: Wind,
+});
+
+app.mount("#app");
